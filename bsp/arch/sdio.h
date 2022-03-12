3f4183888655565c620a4a1d5d83a684f860c455
AirMaxSys <gy520813@live.com>
2022-03-12 21:26:02 +0800

    add arch sdio dirver

---
+++ bsp/arch/sdio.h
@@ -0,0 +1,16 @@
+#ifndef __SDIO_H__
+#define __SDIO_H__
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+#include "stm32h750xx.h"
+
+void mcu_sdio_init(void);
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
